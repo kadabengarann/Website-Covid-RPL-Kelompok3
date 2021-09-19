e7388089ead404e7090d5fa852e41565714d0723
kadabengarann <rio27dh@gmail.com>
2021-09-19 21:37:44 +0800

    fix landing page

--- index.php
+++ index.php
@@ -17,8 +17,8 @@ Coded by www.creative-tim.com
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="./vendor/argon-design-system-master/assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="./vendor/argon-design-system-master/assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="./vendors/argon-design-system-master/assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./vendors/argon-design-system-master/assets/img/favicon.png">
   <title>
     Website Covid Kel3
   </title>
@@ -26,13 +26,13 @@ Coded by www.creative-tim.com
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
   <!-- Nucleo Icons -->
-  <link href="./vendor/argon-design-system-master/assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="./vendor/argon-design-system-master/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="./vendors/argon-design-system-master/assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="./vendors/argon-design-system-master/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- Font Awesome Icons -->
-  <link href="./vendor/argon-design-system-master/assets/css/font-awesome.css" rel="stylesheet" />
-  <link href="./vendor/argon-design-system-master/assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="./vendors/argon-design-system-master/assets/css/font-awesome.css" rel="stylesheet" />
+  <link href="./vendors/argon-design-system-master/assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
-  <link href="./vendor/argon-design-system-master/assets/css/argon-design-system.css?v=1.2.2" rel="stylesheet" />
+  <link href="./vendors/argon-design-system-master/assets/css/argon-design-system.css?v=1.2.2" rel="stylesheet" />
 </head>
 
 <body class="landing-page">
@@ -247,21 +247,21 @@ Coded by www.creative-tim.com
     </footer>
   </div>
   <!--   Core JS Files   -->
-  <script src="./vendor/argon-design-system-master/assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/bootstrap-switch.js"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/bootstrap-switch.js"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/moment.min.js"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/datetimepicker.js" type="text/javascript"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/plugins/bootstrap-datepicker.min.js"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/moment.min.js"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/datetimepicker.js" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/plugins/bootstrap-datepicker.min.js"></script>
   <!-- Control Center for Argon UI Kit: parallax effects, scripts for the example pages etc -->
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <script src="./vendor/argon-design-system-master/assets/js/argon-design-system.min.js?v=1.2.2" type="text/javascript"></script>
+  <script src="./vendors/argon-design-system-master/assets/js/argon-design-system.min.js?v=1.2.2" type="text/javascript"></script>
   <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
   <script>
     window.TrackJS &&
