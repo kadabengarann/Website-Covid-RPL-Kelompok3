018c7745c81584a0099149b2e82e5a60f8b5eb5d
Ardhy Satrio <rio27dh@gmail.com>
2021-09-20 01:50:00 +0800

    update connection

--- services/connection.php
+++ services/connection.php
@@ -1,9 +1,9 @@
 <?php
 try {
     $pdo_conn = new PDO(
-        'mysql:host=localhost;dbname=covid',
-        'root',
-        '',
+        'mysql:host=botkgkqmgyoiovycsvln-mysql.services.clever-cloud.com;dbname=botkgkqmgyoiovycsvln',
+        'uj1qnl4rlq8uzsk2',
+        'k8BXQj9UHy2OnC8B4GKy',
         array(PDO::ATTR_PERSISTENT => true)
     );
 } catch (PDOException $e) {
