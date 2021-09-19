36e27f49002ff87a78a0b930305c9961b1488ad2
ItsYonasa <yosephp.s1412@gmail.com>
2021-09-20 03:35:28 +0700

    Halaman List Tempat Test

---
+++ HalamanSWAB-PCR/HalamanListTempatTestPCRdanSwab.php
@@ -0,0 +1,56 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <div id="header">
+      <li id="site_name">RPL.com</li>
+      <li id="home" onclick="scrollto('home');">HOME</li>
+      <li onclick="scrollto('contact');">CONTACT</li>
+    </div>
+    <meta charset="utf-8">
+    <title>List RS Penyedia Test</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div id="belakang"></div>
+    <div class="wrapper">
+      <img src="RS Mayapada.jpg" height="200" width="200">
+      <h2>RS Mayapada Jakarta Selatan</h2>
+      <p>Salah satu rumah sakit terbaik di Jakarta Selatan / best hospital in Jakarta Selatan, dengan layanan kesehatan terlengkap & didukung oleh tenaga medis terbaik.
+      <br></br>
+      <p>Mayapada Hospital menyediakan layanan PCR Swab dan Rapid/Serologi Test (menggunakan mesin PCR berteknologi US dan Eropa) untuk mendeteksi keberadaan sel, bakteri, atau virus corona (Covid-19) di dalam tubuh. Layanan ini berlaku di semua unit Mayapada Hospital.</p>
+<br></br>
+      <p>PCR Swab Test : Rp489.000</p>
+      <p>Untuk hasil 24 Jam, apabila sampel diterima sebelum pukul 18.00 WIB. Bila di atas jam 18.00 WIB, </p>
+      <p>maka hasil akan diterima H+1 maksimal pukul 24.00 WIB</p>
+      <br></br>
+      <p>Jam Operasional RS Mayapada Jakarta Selatan:</p>
+        <p>Senin - Minggu: 08.00 - 15.00 WIB</p>
+          </div>
+
+
+    <div class="wrapper">
+      <h2>Uji Coba</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+
+
+    <div class="wrapper">
+      <h2>Uji Coba</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+
+
+    <div class="wrapper">
+      <h2>Uji Coba</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </div>
+    <script type="text/javascript">
+      var bgpattern = document.getElementById("belakang")
+      window.addEventListener("scroll", function(){
+        bgpattern.style.backgroundPosition = +window.
+          pageYOffset+"px";
+      })
+    </script>
+  </body>
+</html>
